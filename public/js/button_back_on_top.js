fccf0abeb1e9d54a65d3eb80d298c6e7735cb792
cyril-philippart <philippart.cyril@gmail.com>
2021-05-22 14:53:08 +0200

    add effect vega and favicon

--- public/js/button_back_on_top.js
+++ public/js/button_back_on_top.js
@@ -7,3 +7,19 @@ $(window).scroll(function() {
     button.removeClass('show');
   }
 });
+
+$("#card_main_img").vegas({
+  slides: [
+      { src: "/pictures/testimage.jpg" },
+      { src: "/pictures/photo_menu.jpg" },
+      { src: "/pictures/photo_melissa.jpg" },
+      { src: "/pictures/photo_devanture.jpg" }
+  ],
+
+  overlay: true,
+    transition: 'fade',
+    transitionDuration: '2000',
+    delay: '8000',
+    animation: 'random',
+    animationDuration: '8000'
+});
