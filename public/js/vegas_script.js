734d8f1279497f49fd6d9dd349680915d404d7ee
cyril-philippart <philippart.cyril@gmail.com>
2021-06-16 22:55:34 +0200

    build page card

---
+++ public/js/vegas_script.js
@@ -0,0 +1,61 @@
+$("#card_main_img").vegas({
+    slides: [
+        { src: "/pictures/testimage.jpg" },
+        { src: "/pictures/photo_menu.jpg" },
+        { src: "/pictures/photo_melissa.jpg" },
+        { src: "/pictures/photo_devanture.jpg" }
+    ],
+  
+    overlay: true,
+      transition: 'fade',
+      transitionDuration: '2000',
+      delay: '8000',
+      animation: 'random',
+      animationDuration: '8000'
+});
+
+$(".img_1").vegas({
+    slides: [
+        { src: "/uploads/pictures_products/1.jpg" },
+        { src: "/uploads/pictures_products/2.jpg" },
+        { src: "/uploads/pictures_products/3.jpg" }
+    ],
+  
+    overlay: true,
+      transition: 'fade',
+      transitionDuration: '1000',
+      delay: '10000',
+      animation: 'random',
+      animationDuration: '10000'
+});
+
+$(".img_2").vegas({
+    slides: [
+        { src: "/pictures/plat 1.jpg" },
+        { src: "/pictures/plat 2.jpg" },
+        { src: "/pictures/plat 3.jpg" }
+    ],
+  
+    overlay: true,
+      transition: 'fade',
+      transitionDuration: '1000',
+      delay: '7000',
+      animation: 'random',
+      animationDuration: '7000'
+});
+
+$(".img_3").vegas({
+    slides: [
+        { src: "/pictures/plat 1.jpg" },
+        { src: "/pictures/plat 2.jpg" },
+        { src: "/pictures/plat 3.jpg" }
+    ],
+  
+    overlay: true,
+      transition: 'fade',
+      transitionDuration: '1000',
+      delay: '7000',
+      animation: 'random',
+      animationDuration: '7000'
+});
+ 
